2f1edb6a320ee51980fe27b56bbdae63bc19297b
Needless <rle85@uclive.ac.nz>
2026-05-06 12:14:17 +1200

    Redundant stuff removed

--- ModalFiles/Burgers.py
+++ ModalFiles/Burgers.py
@@ -7,7 +7,6 @@ X_LEFT = -5.0
 X_RIGHT =  5.0
 T_END =  5.0
 NX = 500
-NT_PLOT = 40
 
 x  = np.linspace(X_LEFT, X_RIGHT, NX)
 dx = x[1] - x[0]
@@ -16,16 +15,14 @@ NT = int(T_END / dt) + 1
 
 u = np.exp(-((x - 1)**2) / 2) - np.exp(-((x + 1)**2) / 2)
 
-plot_every = max(1, NT // NT_PLOT)
 u_history  = []
 t_history  = []
 
 u_curr = u.copy()
 
 for n in range(NT):
-    if n % plot_every == 0:
-        u_history.append(u_curr.copy())
-        t_history.append(n * dt)
+    u_history.append(u_curr.copy())
+    t_history.append(n * dt)
 
     u_pos = np.maximum(u_curr, 0)
     u_neg = np.minimum(u_curr, 0)
@@ -51,10 +48,7 @@ X_mesh, T_mesh = np.meshgrid(x, t_history)
 fig = plt.figure(figsize=(10, 8))
 ax = fig.add_subplot(111, projection='3d')
 
-surf = ax.plot_surface(
-    X_mesh, T_mesh, u_history,
-    cmap='plasma'
-)
+surf = ax.plot_surface(X_mesh, T_mesh, u_history, cmap='plasma')
 
 ax.set_xlabel('x')
 ax.set_ylabel('time t')
@@ -75,18 +69,15 @@ cmap_slices = plt.cm.viridis
 colors = cmap_slices(np.linspace(0, 1, N_SLICES))
 
 for colour, idx in zip(colors, slice_ids):
-    ax2.plot(x, u_history[idx], color=colour, linewidth=1.8,
-             label=f't = {t_history[idx]:.2f}')
+    ax2.plot(x, u_history[idx], color=colour, label=f't = {t_history[idx]:.2f}')
 
-ax2.set_xlabel('x', fontsize=13)
-ax2.set_ylabel('u(x, t)', fontsize=13)
-ax2.set_title(
-    '1D Burgers Equation Time Slices'
-)
+ax2.set_xlabel('x')
+ax2.set_ylabel('u(x, t)')
+ax2.set_title('1D Burgers Equation Time Slices')
 
 ax2.legend(loc='upper right', title='Time')
 ax2.set_xlim(X_LEFT, X_RIGHT)
-ax2.grid(True, alpha=0.3)
+ax2.grid(True)
 
 plt.tight_layout()
 plt.show()
